3e235f6aa9cc8ce1f3a508a98bd7db775dc181cf
Brandon Gallagher <200454237@ba-a251-2332.student.georgianc.on.ca>
2020-07-14 13:20:47 -0400

    Added Login View Controller for testing

---
+++ Project/brandon/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Project
+//
+//  Created by Brandon Gallagher on 2020-07-14.
+//  Copyright © 2020 Brandon Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
