7a140ef442fbaaf256e516f04a297fdfca359f76
Brandon Gallagher <200454237@ba-a251-2332.student.georgianc.on.ca>
2020-07-14 13:08:31 -0400

    initial commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Brandon Gallagher on 2020-07-14.
+//  Copyright © 2020 Brandon Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
