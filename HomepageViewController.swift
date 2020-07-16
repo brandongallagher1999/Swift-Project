53c27988643c475933a3e4505792cc0fadc8d3d6
Elias Aguilera Yambay <200393648@ba-a251-2315.student.georgianc.on.ca>
2020-07-15 20:06:30 -0400

    homepage first commit

---
+++ HomepageViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HomepageViewController.swift
+//  Project
+//
+//  Created by Elias Aguilera Yambay on 2020-07-15.
+//  Copyright © 2020 Brandon Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    
+    
+    @IBAction func AddTitle(_ sender: Any) {
+    }
+    
+    
+    @IBAction func AddDetails(_ sender: Any) {
+    }
+    
+    
+    @IBAction func shareInfo(_ sender: Any) {
+    }
+    
+    
+    @IBAction func SubmitButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
